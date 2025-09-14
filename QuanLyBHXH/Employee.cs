0c3c408d6e5c6027238442c4730813a733e705c3
KhoaSuyThan <khoanv249@gmail.com>
2025-09-14 17:00:26 +0700

    Update Employee.cs

--- QuanLyBHXH/Employee.cs
+++ QuanLyBHXH/Employee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -38,7 +38,24 @@ namespace QuanLyBHXH
 
         public override string ToString()
         {
-            return $"{ID} | {Name} | {Gender} | {DateOfBirth:yyyy-MM-dd} | Giai đoạn: {Periods.Count}";
+            return $"{ID} | {Name} | {Gender} | {DateOfBirth:yyyy-MM-dd} | Giai doan: {Periods.Count}";
+        }
+        public double TotalContributedWithFactor(Dictionary<int, double> factorByYear = null)
+        {
+            double total = 0.0;
+            factorByYear = factorByYear ?? new Dictionary<int, double>();
+
+            foreach (var p in Periods)
+            {
+                foreach (var ym in p.IterateMonths())
+                {
+                    int y = ym.year;
+                    double factor = factorByYear.ContainsKey(y) ? factorByYear[y] : 1.0;
+                    total += p.MonthlySalary * factor;
+                }
+            }
+
+            return total;
         }
     }
 }
